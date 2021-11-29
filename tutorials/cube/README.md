3699f19fb2a35a533c575c78f5984aa3996b13ec
Stefan Pirker <stefan.pirker@jku.at>
2021-11-29 09:26:20 +0100

    new tutorial

---
+++ tutorials/cube/README.md
@@ -0,0 +1,54 @@
+# Tundish Tutorial
+
+## Contents
+
+- Compile specific (non rCFD) udf's for full CFD
+- Prepares rCFD database on a 1.2M grid.
+- Runs rCFD considering (i) incoming drifting particles, (ii) change in incoming steel grade 
+and (iii) change in incoming temperature
+
+## Prepare full CFD
+
+- start Ansys/fluent by e.g. *fluent 3ddp -t12 &* from this folder
+- read tundish_tutorial.scm
+- type into fluent console (cube_prep)
+
+- full CFD udf's will be built into /libudf_cube
+
+- close Ansys/fluent
+
+## Prepare rCFD
+
+- start Ansys/fluent by e.g. *fluent 3ddp -t12 &* from this folder
+- read cube_tutorial.scm
+- type into fluent console (rcfd_p1), followed by (rcfd_p1), ..., (rcfd_p11); or simply (rcfd_prep)
+
+- c2c patterns will be stored into ./data/c2c
+- jump vector will be stored into ./rec
+
+- close Ansys/fluent
+
+## Run rCFD
+
+- start Ansys/fluent with the same number of threads from this folder.
+- read tundish_tutorial.scm
+- type into fluent console (rcfd_r1), followed by (rcfd_r1), ..., (rcfd_r7); or simply (rcfd_run)
+
+- balances will be stored into ./post
+- run transcript will be stored into ./post
+- animation snap-shots will be stored into ./post
+
+- close Ansys/fluent
+
+## Known Limitations
+
+- (rcfd_run) works only after (rcfd_prep)
+- Ansys/fluent has to be closed between *prepare full CFD*, *prepare rCFD* and *run rCFD*
+- missing cross-partition diffusion
+
+## License
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.html)
+
+- This software is distributed under the [GNU General Public License](https://www.gnu.org/licenses/gpl-3.0.html).
+- Copyright © 2021 JKU Linz
---
+++ tutorials/cube/user_src/rCFD_user.h
@@ -0,0 +1,361 @@
+#ifndef RCFD_USER
+#define RCFD_USER
+
+#include "rCFD_types.h"
+#include "rCFD_globals.h"
+#include "rCFD_defaults.h"
+#include "rCFD_macros.h"
+
+/* (C)  2021 
+    Stefan Pirker
+    Particulate Flow Modelling
+    Johannes Kepler University, Linz, Austria
+    www.particulate-flow.at
+*/  
+
+    
+    /* user Names */
+    /*************************************************************************************/ 
+
+#if 1
+        /* names of phases */
+        enum{
+            air,
+            number_of_phase_names
+        };
+
+        /* names of phase user vars - phase_0 */
+        enum{
+            number_of_phase0_user_vars_names
+        };
+
+        /* names of data - phase_0 */
+        enum{
+            c_drift_0p0,
+            c_drift_0p001,
+            c_drift_0p002,
+            number_of_phase0_data_names
+        };
+
+#endif
+    
+    /* user set Dicts */
+    /*************************************************************************************/
+    
+#if 1
+    
+    void rCFD_user_set_Solver_Dict(Solver_Dict_type *Solver_Dict)
+    {
+        Solver_Dict->max_number_of_cells_per_time_step =    30;
+
+        Solver_Dict->global_time_step =                     0.005;   
+		
+		Solver_Dict->time_steps_per_monitoring_interval =	25;
+		
+        Solver_Dict->number_of_frames =                     10;         
+
+        Solver_Dict->number_of_runs =                       1; 
+        
+        Solver_Dict->data_drifting_on =                     1; 
+
+		Solver_Dict->face_swap_max_per_loop =   (1./8.);        
+        Solver_Dict->face_swap_min =            (1./64.);
+        Solver_Dict->face_swap_max_loops =      8;  
+		
+    }   
+
+    void rCFD_user_set_File_Dict(Solver_Dict_type *Solver_Dict, File_Dict_type *File_Dict)
+    {
+
+    }   
+    
+    void rCFD_user_set_Phase_Dict(Solver_Dict_type *Solver_Dict, Phase_Dict_type* Phase_Dict)
+    {
+        int i_phase;
+        
+        loop_phases_ptr{
+            
+            if(i_phase == air){
+                
+                Phase_Dict[i_phase].number_of_data = 3;
+
+                Phase_Dict[i_phase].time_step = Solver_Dict->global_time_step;
+                
+                Phase_Dict[i_phase].density = 1.25;
+            }
+        }
+    }
+
+    void rCFD_user_set_Tracer_Dict(Solver_Dict_type *Solver_Dict, Tracer_Dict_type *Tracer_Dict)
+    {
+
+    }
+
+    void rCFD_user_set_Norm_Dict(Solver_Dict_type *Solver_Dict, Norm_Dict_type *Norm_Dict)
+    {
+
+    }
+
+    void rCFD_user_set_Rec_Dict(Solver_Dict_type *Solver_Dict, Rec_Dict_type *Rec_Dict)
+    {
+
+    }
+    
+    void rCFD_user_set_Data_Dict(Solver_Dict_type *Solver_Dict, Phase_Dict_type *Phase_Dict, Data_Dict_type **Data_Dict)
+    {
+        int i_phase, i_data;
+        
+        loop_phases_ptr{
+            
+            loop_data{
+                
+                if((i_phase == air) && (i_data == c_drift_0p0)){
+                    
+                    Data_Dict[i_phase][i_data].type = generic_data;
+                }
+
+                if((i_phase == air) && (i_data == c_drift_0p001)){
+                    
+                    Data_Dict[i_phase][i_data].type = generic_data;
+
+                    Data_Dict[i_phase][i_data].drifting_data = 1;
+                    
+                    Data_Dict[i_phase][i_data].drift_velocity = (double*)malloc( 3 * sizeof(double));
+                    
+                    Data_Dict[i_phase][i_data].drift_velocity[0] = 0.0;
+
+                    Data_Dict[i_phase][i_data].drift_velocity[1] = 0.0;
+
+                    Data_Dict[i_phase][i_data].drift_velocity[2] = -0.015;
+                }
+
+                if((i_phase == air) && (i_data == c_drift_0p002)){
+                    
+                    Data_Dict[i_phase][i_data].type = generic_data;
+
+                    Data_Dict[i_phase][i_data].drifting_data = 1;
+                    
+                    Data_Dict[i_phase][i_data].drift_velocity = (double*)malloc( 3 * sizeof(double));
+                    
+                    Data_Dict[i_phase][i_data].drift_velocity[0] = 0.0;
+
+                    Data_Dict[i_phase][i_data].drift_velocity[1] = 0.0;
+
+                    Data_Dict[i_phase][i_data].drift_velocity[2] = -0.03;
+                }
+            }
+        }
+    }
+    
+    void rCFD_user_set_Balance_Dict(Solver_Dict_type *Solver_Dict, Phase_Dict_type *Phase_Dict, Balance_Dict_type **Balance_Dict)
+    {
+       int i_phase, i_data;
+
+        loop_phases_ptr{
+
+            loop_data{
+            
+				/*Balance_Dict[i_phase][i_data].max_correction_loops = 5;*/
+
+                Balance_Dict[i_phase][i_data].write_balance_to_file = 1;
+
+                Balance_Dict[i_phase][i_data].write_balance_to_file_interval = Solver_Dict->number_of_runs;
+            }
+        }
+    }
+
+    void rCFD_user_set_Topo_Dict(Solver_Dict_type *Solver_Dict, Topo_Dict_type *Topo_Dict)
+    {
+        
+    }
+
+    void rCFD_user_set_Cell_Dict(Solver_Dict_type *Solver_Dict, Cell_Dict_type *Cell_Dict, const short i_layer)
+    {
+
+    }
+    
+    void rCFD_user_set_Face_Dict(Solver_Dict_type *Solver_Dict, Face_Dict_type *Face_Dict, const short i_layer)
+    {
+        
+    }
+#endif
+
+    /* user init */
+    /*************************************************************************************/
+    
+#if 1   
+    void rCFD_user_pre_proc(Solver_Dict_type *Solver_Dict, Phase_Dict_type *Phase_Dict, Topo_Dict_type *Topo_Dict, Cell_type *C)
+    {
+        Domain  *d=Get_Domain(1);
+        Thread  *t;
+
+        int i_phase, i_cell, i_UDMI;
+        
+        thread_loop_c(t,d){if(FLUID_CELL_THREAD_P(t)){begin_c_loop_int(i_cell, t){
+            
+            i_UDMI = 0;     /* start index */
+            
+            loop_phases_ptr{
+                
+                C_UDMI(i_cell, t, i_UDMI) = C->crossing_time[i_phase][i_cell];
+
+                C_UDMI(i_cell, t, (i_UDMI+1)) = C->average_velocity[i_phase][i_cell];
+                
+                i_UDMI += 2;
+            }
+            
+        }end_c_loop_int(i_cell, t)}}        
+    }
+
+    void rCFD_user_set_recurrence_time_step(Solver_Dict_type *Solver_Dict, Phase_Dict_type* Phase_Dict)
+    {
+    }
+        
+    void rCFD_user_init_Data(Solver_Dict_type *Solver_Dict, Balance_type** Balance, Phase_Dict_type* Phase_Dict, Data_Dict_type** Data_Dict, 
+        Topo_Dict_type *Topo_Dict, Cell_type *C, const short i_layer)
+    {
+        int     i_phase, i_cell, i_data;
+
+        loop_phases_ptr{
+        
+            loop_cells_ptr{
+                             
+                loop_data{
+            
+                    C->data[i_phase][i_cell][i_data] = 0.0; 
+                }
+            }
+        }
+        
+        Message0("\n\n...rCFD_user_init_Data\n");               
+    }
+
+#endif  
+    
+    /* user accesses Data */
+    /*************************************************************************************/
+
+#if 1
+    
+    void rCFD_user_access_data_before_shift(Balance_type** Balance, Phase_Dict_type* Phase_Dict, 
+        Topo_Dict_type *Topo_Dict, Cell_type *C, Rec_type *Rec, 
+        const short i_phase, const short i_layer)
+    {
+#if RP_NODE
+        int     i_cell, i_data, i_dim;
+
+        double  x[3];
+        
+        double  radius_in, tracer_gas_flowrate, V_in, V_in_global; 
+
+		V_in = 0.0;
+		
+		tracer_gas_flowrate = 1.0e-3;   /* (kg/s) */			
+        
+        /* inflow bc volume */
+		loop_int_cells_ptr{
+            
+            loop_dim{
+                
+                x[i_dim] = C->x[i_cell][i_dim];
+            }
+            
+			radius_in = sqrt(x[0]*x[0] + x[1]*x[1]);
+			
+			if((radius_in <= 0.01) && (x[2] < 0.075)){
+
+				V_in += C->volume[i_cell];
+			}
+		}
+				
+		V_in_global = PRF_GRSUM1(V_in);
+		
+        loop_int_cells_ptr{
+            
+            /* coord's */
+            loop_dim{
+                
+                x[i_dim] = C->x[i_cell][i_dim];
+            }
+            
+            /* inflow bc */
+            {				   
+			    radius_in = sqrt(x[0]*x[0] + x[1]*x[1]);
+				
+                if((radius_in <= 0.01) && (x[2] < 0.075)){
+
+					loop_data{
+						
+						C->data[i_phase][i_cell][i_data] += C->volume[i_cell]/V_in_global * tracer_gas_flowrate * Phase_Dict[i_phase].time_step;  /* (kg) */
+
+						Balance[i_phase][i_data].mass_source +=  C->volume[i_cell]/V_in_global * tracer_gas_flowrate * Phase_Dict[i_phase].time_step;  /* (kg) */
+					}
+				}	
+            }
+            
+            /* outflow bc */
+            {
+                if(x[0] > 0.6){
+
+					loop_data{
+						
+						Balance[i_phase][i_data].mass_source -=  C->data[i_phase][i_cell][i_data];  /* (kg) */
+
+						C->data[i_phase][i_cell][i_data] = 0.0;  /* (kg) */
+					}
+				}	
+            }
+        }           
+#endif
+    }
+
+    void rCFD_user_access_data_after_swap(Balance_type** Balance, Phase_Dict_type* Phase_Dict, 
+        Data_Dict_type **Data_Dict, Topo_Dict_type *Topo_Dict, Cell_type *C, 
+        const short i_phase, const short i_layer)
+    {
+
+    }
+
+    void rCFD_user_post(Solver_Dict_type *Solver_Dict, Phase_Dict_type *Phase_Dict, Topo_Dict_type *Topo_Dict, Cell_type *C)
+    {
+        Domain  *d=Get_Domain(1);
+        Thread  *t;
+
+        int i_phase, i_cell, i_data, i_UDMI;
+        
+        thread_loop_c(t,d){if(FLUID_CELL_THREAD_P(t)){begin_c_loop_int(i_cell, t){
+            
+            i_UDMI = 0;     /* start index */
+            
+            loop_phases_ptr{
+                
+                loop_data{
+                
+                    C_UDMI(i_cell, t, i_UDMI) = C->data[i_phase][i_cell][i_data];
+                    
+                    i_UDMI++;
+                }
+            }
+            
+        }end_c_loop_int(i_cell, t)}}        
+    }
+#endif
+
+    /* user sub models */
+    /*************************************************************************************/
+
+#if 1
+    
+    double rCFD_user_set_random_walk_velocity(Solver_Dict_type *Solver_Dict, Thread *t, int i_cell)
+    {
+        return 0.0;
+    }   
+
+    void rCFD_user_set_Norm(Solver_Dict_type *Solver_Dict, Norm_type *Norms)
+    {
+        
+    }
+    
+#endif  
+
+#endif

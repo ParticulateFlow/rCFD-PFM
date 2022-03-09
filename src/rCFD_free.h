052134f67cb6cde8107cded6c2aa9f1b7de03e74
Daniel Queteschiner <daniel.queteschiner@jku.at>
2022-03-09 15:59:28 +0100

    remove redundant checks against NULL
    free(NULL) is a no-op as per instructed by the C standard

--- src/rCFD_free.h
+++ src/rCFD_free.h
@@ -16,27 +16,18 @@
     {
         int i_state;
 
-        if(Rec.global_frame != NULL){
-
-            free(Rec.global_frame);
-        }
+        free(Rec.global_frame);
 
         free_i_4d(Rec.jumps);
 
-        if(Solver.timestep_width != NULL){
-
-            free(Solver.timestep_width);
-        }
+        free(Solver.timestep_width);
 
 #if RP_NODE
         int     i_phase, i_frame, i_data, i_shift, i_node, i_cell, i_layer;
 
         int     number_of_shifts;
 
-        if(Tracer_Dict.random_walk != NULL){
-
-            free(Tracer_Dict.random_walk);
-        }
+        free(Tracer_Dict.random_walk);
 
         if(Topo.Cell != NULL){
 
@@ -53,25 +44,13 @@
 
                 free_r_2d(_C.crossing_time);
 
-                if(_C.hit_by_other_cell != NULL){
+                free(_C.hit_by_other_cell);
 
-                    free(_C.hit_by_other_cell);
-                }
+                free(_C.island_id);
 
-                if(_C.island_id != NULL){
+                free(_C.weight_after_shift);
 
-                    free(_C.island_id);
-                }
-
-                if(_C.weight_after_shift != NULL){
-
-                    free(_C.weight_after_shift);
-                }
-
-                if(_C.weight_after_swap != NULL){
-
-                    free(_C.weight_after_swap);
-                }
+                free(_C.weight_after_swap);
 
                 free_r_3d(_C.vof);
 
@@ -81,36 +60,21 @@
 
                 free_r_3d(_C.data_swap);
 
-                if(_C.drift_exchange != NULL){
-
-                    free(_C.drift_exchange);
-                }
+                free(_C.drift_exchange);
 
                 free_r_2d(_C.user);
 
-                if(_C.marked != NULL){
+                free(_C.marked);
 
-                    free(_C.marked);
-                }
+                free(_C.parent_cell);
 
-                if(_C.parent_cell != NULL){
-
-                    free(_C.parent_cell);
-                }
-
-                if(_C.number_of_children != NULL){
-
-                    free(_C.number_of_children);
-                }
+                free(_C.number_of_children);
 
                 if(_C.child_index != NULL){
 
                     loop_cells{
 
-                        if(_C.child_index[i_cell] !=  NULL){
-
-                            free(_C.child_index[i_cell]);
-                        }
+                        free(_C.child_index[i_cell]);
                     }
 
                     free(_C.child_index);
@@ -124,15 +88,9 @@
 
             loop_layers{
 
-                if(_F.c0 != NULL){
+                free(_F.c0);
 
-                    free(_F.c0);
-                }
-
-                if(_F.c1 != NULL){
-
-                    free(_F.c1);
-                }
+                free(_F.c1);
 
                 free_r_2d(_F.area);
             }
@@ -140,43 +98,26 @@
             free(Topo.Face);
         }
 
-        if(Topo_Dict.Cell_Dict != NULL){
+        free(Topo_Dict.Cell_Dict);
 
-            free(Topo_Dict.Cell_Dict);
-        }
+        free(Topo_Dict.Face_Dict);
 
-        if(Topo_Dict.Face_Dict != NULL){
 
-            free(Topo_Dict.Face_Dict);
-        }
+        free(Tracer.monitor_counter);
 
-        if(Tracer.monitor_counter != NULL){
-
-            free(Tracer.monitor_counter);
-        }
-
-        if(Tracer.number_of_shifts != NULL){
-
-            free(Tracer.number_of_shifts);
-        }
+        free(Tracer.number_of_shifts);
 
         if(Tracer.shifts != NULL){
 
             loop_phases{
 
-                if(Tracer.shifts[i_phase] != NULL){
-
-                    free(Tracer.shifts[i_phase]);
-                }
+                free(Tracer.shifts[i_phase]);
             }
 
             free(Tracer.shifts);
         }
 
-        if(Norms.norm != NULL){
-
-            free(Norms.norm);
-        }
+        free(Norms.norm);
 
         if(C2Cs != NULL){
 
@@ -190,29 +131,17 @@
 
                             loop_frames{
 
-                                if(C2Cs[_i_C2C].number_of_shifts != NULL){
+                                free(C2Cs[_i_C2C].number_of_shifts);
 
-                                    free(C2Cs[_i_C2C].number_of_shifts);
-                                }
+                                free(C2Cs[_i_C2C].number_of_shifts_in);
 
-                                if(C2Cs[_i_C2C].number_of_shifts_in != NULL){
-
-                                    free(C2Cs[_i_C2C].number_of_shifts_in);
-                                }
-
-                                if(C2Cs[_i_C2C].number_of_shifts_out != NULL){
-
-                                    free(C2Cs[_i_C2C].number_of_shifts_out);
-                                }
+                                free(C2Cs[_i_C2C].number_of_shifts_out);
 
                                 if(C2Cs[_i_C2C].shifts != NULL){
 
                                     loop_layers{
 
-                                        if(C2Cs[_i_C2C].shifts[i_layer] != NULL){
-
-                                            free(C2Cs[_i_C2C].shifts[i_layer]);
-                                        }
+                                        free(C2Cs[_i_C2C].shifts[i_layer]);
                                     }
 
                                     free(C2Cs[_i_C2C].shifts);
@@ -222,10 +151,7 @@
 
                                     loop_layers{
 
-                                        if(C2Cs[_i_C2C].shifts_in[i_layer] != NULL){
-
-                                            free(C2Cs[_i_C2C].shifts_in[i_layer]);
-                                        }
+                                        free(C2Cs[_i_C2C].shifts_in[i_layer]);
                                     }
 
                                     free(C2Cs[_i_C2C].shifts_in);
@@ -235,10 +161,7 @@
 
                                     loop_layers{
 
-                                        if(C2Cs[_i_C2C].shifts_out[i_layer] != NULL){
-
-                                            free(C2Cs[_i_C2C].shifts_out[i_layer]);
-                                        }
+                                        free(C2Cs[_i_C2C].shifts_out[i_layer]);
                                     }
 
                                     free(C2Cs[_i_C2C].shifts_out);
@@ -254,10 +177,7 @@
 
                                         loop_layers{
 
-                                            if(C2Cs[_i_C2C].number_of_shifts_to_node_zero[i_layer] != NULL){
-
-                                                free(C2Cs[_i_C2C].number_of_shifts_to_node_zero[i_layer]);
-                                            }
+                                            free(C2Cs[_i_C2C].number_of_shifts_to_node_zero[i_layer]);
                                         }
 
                                         free(C2Cs[_i_C2C].number_of_shifts_to_node_zero);
@@ -267,10 +187,7 @@
 
                                         loop_layers{
 
-                                            if(C2Cs[_i_C2C].number_of_shifts_from_node_zero[i_layer] != NULL){
-
-                                                free(C2Cs[_i_C2C].number_of_shifts_from_node_zero[i_layer]);
-                                            }
+                                            free(C2Cs[_i_C2C].number_of_shifts_from_node_zero[i_layer]);
                                         }
 
                                         free(C2Cs[_i_C2C].number_of_shifts_from_node_zero);
@@ -284,10 +201,7 @@
 
                                                 for(i_node = 0; i_node < (node_last + 1); i_node++){
 
-                                                    if(C2Cs[_i_C2C].in2out[i_layer][i_node] != NULL){
-
-                                                        free(C2Cs[_i_C2C].in2out[i_layer][i_node]);
-                                                    }
+                                                    free(C2Cs[_i_C2C].in2out[i_layer][i_node]);
                                                 }
 
                                                 free(C2Cs[_i_C2C].in2out[i_layer]);
@@ -346,10 +260,7 @@
 
             loop_phases{
 
-                if(Balance_Dict[i_phase] != NULL){
-
-                    free(Balance_Dict[i_phase]);
-                }
+                free(Balance_Dict[i_phase]);
             }
 
             free(Balance_Dict);
@@ -363,15 +274,9 @@
 
                     loop_data{
 
-                        if(Data_Dict[i_phase][i_data].drift_velocity != NULL){
+                        free(Data_Dict[i_phase][i_data].drift_velocity);
 
-                            free(Data_Dict[i_phase][i_data].drift_velocity);
-                        }
-
-                        if(Data_Dict[i_phase][i_data].user != NULL){
-
-                            free(Data_Dict[i_phase][i_data].user);
-                        }
+                        free(Data_Dict[i_phase][i_data].user);
                     }
 
                     free(Data_Dict[i_phase]);
@@ -386,10 +291,7 @@
 
             loop_phases{
 
-                if(Phase_Dict[i_phase].user != NULL){
-
-                    free(Phase_Dict[i_phase].user);
-                }
+                free(Phase_Dict[i_phase].user);
             }
 
             free(Phase_Dict);

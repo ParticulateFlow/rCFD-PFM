2116f3426dcb92bb2ba7683ed90ae9237328babe
Stefan Pirker <stefan.pirker@jku.at>
2022-06-02 08:29:38 +0200

    Update rCFD_init.h
    - bug fix: somehow lost user_set function calls (needed for _C.user vars)

--- src/rCFD_init.h
+++ src/rCFD_init.h
@@ -114,13 +114,26 @@ void init_all(void)
         rCFD_user_set_Topo_Dict();
 
 #if RP_NODE
-        Topo_Dict.Cell_Dict = (Cell_Dict_type*)malloc(Solver_Dict.number_of_layers * sizeof(Cell_Dict_type));
 
-        Topo_Dict.Face_Dict = (Face_Dict_type*)malloc(Solver_Dict.number_of_layers * sizeof(Face_Dict_type));
+        Topo_Dict.Cell_Dict = (Cell_Dict_type*)malloc(Solver_Dict.number_of_layers * sizeof(Cell_Dict_type));
 
         rCFD_default_Cell_Dict_L0();
 
+        int i_layer;
+        
+        loop_layers{
+            
+            rCFD_user_set_Cell_Dict(i_layer);
+        }
+        
+        Topo_Dict.Face_Dict = (Face_Dict_type*)malloc(Solver_Dict.number_of_layers * sizeof(Face_Dict_type));
+
         rCFD_default_Face_Dict_L0();
+        
+        loop_layers{
+            
+            rCFD_user_set_Face_Dict(i_layer);
+        }       
 #endif
     }
 

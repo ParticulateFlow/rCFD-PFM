d528130cc436314f0bf2f8d633860ba1658a353f
Daniel <daniel.queteschiner@jku.at>
2022-10-13 17:03:38 +0200

    register user defined functions in tutorial cases
    register case specific functions with function pointer struct and
    remove unneeded functions (using default functions instead)

--- tutorials/fluidized_bed/user_src/rCFD_user.h
+++ tutorials/fluidized_bed/user_src/rCFD_user.h
@@ -74,11 +74,6 @@
 
     }
 
-    void rCFD_user_set_File_Dict(void)
-    {
-
-    }
-
     void rCFD_user_set_Phase_Dict(void)
     {
 #if RP_NODE
@@ -105,21 +100,6 @@
             }
         }
 #endif
-    }
-
-    void rCFD_user_set_Tracer_Dict(void)
-    {
-
-    }
-
-    void rCFD_user_set_Norm_Dict(void)
-    {
-
-    }
-
-    void rCFD_user_set_Rec_Dict(void)
-    {
-
     }
 
     void rCFD_user_set_Data_Dict(void)
@@ -198,26 +178,13 @@
 #endif
     }
 
-    void rCFD_user_set_Topo_Dict(void)
-    {
-
-    }
-
-    void rCFD_user_set_Cell_Dict(const short i_layer)
-    {
-
-    }
-
-    void rCFD_user_set_Face_Dict(const short i_layer)
-    {
-
-    }
 #endif
 
     /* user init */
     /*************************************************************************************/
 
 #if 1
+
     void rCFD_user_pre_proc(void)
     {
 #if RP_NODE
@@ -295,12 +262,6 @@
     /*************************************************************************************/
 
 #if 1
-
-    short rCFD_user_set_layer(const short i_layer)
-    {
-
-        return 0;
-    }
 
     void rCFD_user_access_data_before_shift(const short i_phase, const short i_layer)
     {
@@ -385,11 +346,6 @@
             Message0("\n\nUser before Shift: i_phase %d, i_layer %d", i_phase, i_layer);
         }
 #endif
-    }
-
-    void rCFD_user_access_data_after_swap(const short i_phase, const short i_layer)
-    {
-
     }
 
     void rCFD_user_post()
@@ -507,23 +463,22 @@
         }
 #endif
     }
+
 #endif
 
-    /* user sub models */
+    /* register user functions */
     /*************************************************************************************/
 
-#if 1
-
-    double rCFD_user_set_random_walk_velocity(void)
+    void rCFD_user_register_functions()
     {
-        return 0.0;
+        REGISTER_RCFD_UDF( rCFD_user_set_Solver_Dict );
+        REGISTER_RCFD_UDF( rCFD_user_set_Phase_Dict );
+        REGISTER_RCFD_UDF( rCFD_user_set_Data_Dict );
+        REGISTER_RCFD_UDF( rCFD_user_set_Balance_Dict );
+        REGISTER_RCFD_UDF( rCFD_user_pre_proc );
+        REGISTER_RCFD_UDF( rCFD_user_init_Data );
+        REGISTER_RCFD_UDF( rCFD_user_access_data_before_shift );
+        REGISTER_RCFD_UDF( rCFD_user_post );
     }
-
-    void rCFD_user_set_Norm(void)
-    {
-
-    }
-
-#endif
 
 #endif

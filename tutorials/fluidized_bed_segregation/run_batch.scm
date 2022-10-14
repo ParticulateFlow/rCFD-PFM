4470548ccd7d916b8825c0671b115aea838c5f3e
Daniel <daniel.queteschiner@jku.at>
2022-10-14 15:17:20 +0200

    update .scm files of fluidized_bed_segregation tutorial
    clean up and change file names to existing files

--- tutorials/fluidized_bed_segregation/run_batch.scm
+++ tutorials/fluidized_bed_segregation/run_batch.scm
@@ -3,18 +3,16 @@
 ;;  Particulate Flow Modelling
 ;;  Johannes Kepler University, Linz, Austria
 ;;  www.particulate-flow.at
-
+;;
 (ti-menu-load-string (format  #f "!cp ~a/~a.cas.h5 ." "./ansys_fluent" "quad_80k"))
 (ti-menu-load-string (format  #f "!cp ~a/~a.dat.h5 ." "./ansys_fluent" "quad_80k"))
-
+;;
 (ti-menu-load-string (format  #f "!cp ~a/*.c ." "../../src"))
 (ti-menu-load-string (format  #f "!cp ~a/*.h ." "../../src"))
 (ti-menu-load-string (format  #f "!cp ~a/*.h ." "./user_src"))
-
 (ti-menu-load-string (format  #f "!mv ./rCFD_user_batch.h ./rCFD_user.h"))
-
 (ti-menu-load-string "!rm -r libudf_rcfd_run")
-
+;;
 (ti-menu-load-string "/define/user-defined/compiled-functions compile
     libudf_rcfd_run
     yes
@@ -28,36 +26,29 @@
     rCFD_macros.h
     rCFD_init.h
     rCFD_layer.h
-    rCFD_utilities.h
     rCFD_memory.h
     rCFD_free.h
     \"\" ")
-
+;;
 (ti-menu-load-string (format #f "/file/read-case-data ./~a.cas.h5 OK" "quad_80k"))
-
 (ti-menu-load-string "/define/user-defined/user-defined-memory 30 q" )
-
 (ti-menu-load-string "/define/user-defined/compiled-functions load \"libudf_rcfd_run\"")
-
 (ti-menu-load-string "/define/user-defined/execute-on-demand \"rCFD_init_all::libudf_rcfd_run\"")
-
+;;
 (ti-menu-load-string "/define/user-defined/execute-on-demand \"rCFD_read_C2Cs::libudf_rcfd_run\"")
-
+;;
 (ti-menu-load-string "/define/user-defined/execute-on-demand \"rCFD_run::libudf_rcfd_run\"")
-
+;;
 (ti-menu-load-string "/define/user-defined/execute-on-demand \"rCFD_free_all::libudf_rcfd_run\"")
-
+;;
 (ti-menu-load-string "!mv ./run_batch.trn ./post")
-
-(ti-menu-load-string "!mv ./Run.trn ./post")
-
 (ti-menu-load-string "!rm -r ./*.c")
 (ti-menu-load-string "!rm -r ./*.h")
 (ti-menu-load-string "!rm -r ./*.h5")
-
 (ti-menu-load-string "!rm -r ./log")
 (ti-menu-load-string "!rm -r ./*.log")
-
 (ti-menu-load-string "!rm -r ./*.trn")
-
 (ti-menu-load-string "!rm -r ./*.sh")
+;;
+(ti-menu-load-string "/exit OK")
+
